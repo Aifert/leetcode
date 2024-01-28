dff0bba77a96fec5c760f8979507fa7ac1ea37a5
Aifert Yet <aifertyet0628@gmail.com>
2024-01-28 21:32:34 +0800

    done pascals

--- pascals.py
+++ pascals.py
@@ -2,4 +2,14 @@
 
 
 def generate(numRows):
-    return
+    result = [[1]]
+
+    for i in range(numRows - 1):
+        # building a temporary array so we don't modify the actual result
+        temp = [0] + result[-1] + [0]
+        row = []
+        for j in range(len(result[-1]) + 1):
+            row.append(temp[j] + temp[j + 1])
+        result.append(row)
+
+    return result
